80d7e4f1b57787ba506d010616cf90bb631cc053
IvanCherniienko <111380678+IvanCherniienko@users.noreply.github.com>
2023-04-03 18:00:29 +0300

    release

--- index.js
+++ index.js
@@ -1,37 +1,36 @@
-const modal = document.querySelector('#myModal');
-const body = document.querySelector('.body');
-const openModal = document.querySelector('.js-button');
-
-const cancel = document.querySelector('.modal__cancel');
-const subscribe = document.querySelector('.modal__subscribe');
+const modal = document.querySelector('#myModal')
+const body = document.querySelector('.body')
+const openModal = document.querySelector('.js-button')
 
 openModal.addEventListener('click', onOpenModalClick);
-// cancel.addEventListener('click', onCancelClick);
-// subscribe.addEventListener('click', onSubscribeClick);
 
 function onOpenModalClick(event) {
   if (!body.classList.contains('darkened')) {
     modal.classList.toggle('visible')
     body.classList.add('darkened')
     openModal.classList.add('add-to-btn')
+
+    const cancelBtn = document.querySelector('.modal__cancel')
+    cancelBtn.addEventListener('click', function () {
+      modal.classList.remove('visible')
+      body.classList.remove('darkened')
+      openModal.classList.remove('add-to-btn')
+    })
+
+    const subscribeBtn = document.querySelector('.modal__subscribe')
+    subscribeBtn.addEventListener('click', function () {
+      modal.classList.remove('visible')
+      body.classList.remove('darkened')
+      openModal.classList.remove('add-to-btn')
+    })
   }
   event.preventDefault()
 }
 
-
-// function onCancelClick() {
-//     modal.classList.toggle('visible');
-//     body.classList.remove('darkened');
-// }
-
-// function onSubscribeClick() {
-//     // Handle subscribe click here
-// }
-
 body.addEventListener('click', function (event) {
     if (!modal.contains(event.target) && event.target !== openModal) {
-        modal.classList.remove('visible');
-        body.classList.remove('darkened');
+        modal.classList.remove('visible')
+        body.classList.remove('darkened')
         openModal.classList.remove('add-to-btn')
     }
 })
